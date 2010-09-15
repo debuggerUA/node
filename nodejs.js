dc62545d57140b315493d570b793cf52f3ba71d4
Denis Babak <debugger88@gmail.com>
2010-09-15 12:53:13 +0300

    Now works with redis DB. And no UI, only JSON

--- nodejs.js
+++ nodejs.js
@@ -1,42 +1,35 @@
 var http = require('http');
 var url = require('url');
-var multipart = require('./multipart/lib/multipart.js');
-var parser = multipart.parser();
+//var multipart = require('./multipart/lib/multipart.js');
+//var parser = multipart.parser();
 var querystring = require('querystring');
 var sys = require('sys');
+var client = require('./redis-node-client/lib/redis-client.js').createClient();
 
-var base = new Array();
-base['server'] = 'node.js';
 http.createServer(function (req, res) {
 	if (req.method == 'GET') {
-  	try {
-  		parsedUri = url.parse(req.url, parseQueryString = true);
-  		if (parsedUri.pathname.slice(1) != '') {
-				res.writeHead(200, {'Content-Type': 'application/json'});
-				resp = {'key': base[parsedUri.pathname.slice(1)], 'success': true};
-				res.write(JSON.stringify(resp));
-  		}
-  		else {
-  			res.writeHead(200, {'Content-Type': 'text/html'});
-  			res.write('<html><body><form action="/" method="post"><input type="text" name="key"><input type="text" name="value"><input type="submit" value="post"></form></body></html>');
-  		}
-  		res.end();
-  		console.log(req.url);
-  	} 
-  	catch (Exc) {
-  		res.writeHead(500, {'Content-Type': 'application/json'});
-			resp = {'success': false};
-  		res.end(JSON.stringify(resp));
-		console.log(Exc.message);
-  	}
-  }
+			res.writeHead(200, {'Content-Type': 'application/json'});
+  			parsedUri = url.parse(req.url, parseQueryString = true);
+  			if (parsedUri.pathname.slice(1) != '' && parsedUri.pathname.slice(1) != 'favicon.ico') {
+					client.get(parsedUri.pathname.slice(1), function(err, response){
+						res.write(JSON.stringify({'key': response.toString(), 'success': true}));
+						res.end();
+						});
+  			}
+  			else {
+					resp = {'success': true};
+					res.write(JSON.stringify(resp));
+					res.end();
+  			}
+	} 
 	if(req.method == 'POST')
 	{
 		post_handler(req, function(request_data)
       {
-				base[request_data.key] = request_data.value;
-				res.writeHead(200, {'Content-Type': 'text/html'});
-				res.write('<html><body><form action="/" method="post"><input type="text" name="key"><input type="text" name="value"><input type="submit" value="post"></form></body></html>');
+				res.writeHead(200, {'Content-Type': 'application/json'});
+				parsedUri = url.parse(req.url, parseQueryString = true);
+				client.set(parsedUri.pathname.slice(1), request_data.keyvalue);
+				res.write(JSON.stringify({'success': true}));
 				res.end();
       });
 	}
--- tornading.py
+++ tornading.py
@@ -2,6 +2,7 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import redis
 
 from tornado.options import define, options
 
@@ -9,29 +10,38 @@ from django.utils import simplejson
 
 define("port", default=8000, help="run on the given port", type=int)
 
-base = {'server': 'tornado',}
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 class ReqHandler(tornado.web.RequestHandler):
     def get(self, key):
-        if key in base:
-            data = {'response': base[key], 'success': True}
-        else:
+        try:
+            data = {key: r.get(key), 'success': True}
+        except:
+            data = {'success': False}
+        self.write(simplejson.dumps(data))
+    def post(self, key):
+        try:
+            r.set('key', self.get_argument('keyvalue'))
+            data = {'success': True}
+        except:
             data = {'success': False}
         self.write(simplejson.dumps(data))
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('<html><body><form action="/" method="post"><input type="text" name="key"><br><input type="text" name="value"><br><input type="submit" value="Put"></form></body></html>')
-    def post(self):
-        base[self.get_argument("key")] = self.get_argument("value")
-        self.redirect('/')
+        data = {'success': True}
+        self.write(simplejson.dumps(data))
+    
+
 
 def main():
+    r.set('server', 'tornado')
     tornado.options.parse_command_line()
     application = tornado.web.Application([(r"/(\w+)", ReqHandler), (r"/", MainHandler), ])
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
+    
 
 
 if __name__ == "__main__":
